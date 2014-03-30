043f798e4483d4d6133a555d11f3eb9b04b75f3b
nafsadh <nafsadh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-30 13:07:52 +0000

    putting the plugin to the SVN! First commit
    git-svn-id: https://plugins.svn.wordpress.org/credible-names/trunk@884596 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ credible-names.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: Credible Names
+Plugin URI: http://sfaar.net/files/wp/plugin/credible-names/credible-names.zip
+Description: Ensure user's nickname and display name are unique and not a display name or nick name or user name of someone else within the site.
+Version: 0.1.1
+Author: nafSadh Khan
+Author URI: http://nafSadh.com/Khan
+*/
+
+// based on code by  Ashok & Vaughan Montgomery
+// from: http://wordpress.org/support/topic/how-to-make-displayname-nicknames-unique
+// adopted by nafSadh (khan@nafSadh.com)
+// Checks if display_name is already in use as display_name, nickname or login_name of some other user
+// Checks if nickname is already in use as display_name, nickname or login_name of some other user
+/*
+ * adding action when user profile is updated
+ */
+add_action('personal_options_update', 'validate_profile_name');
+add_action('edit_user_profile_update', 'validate_profile_name');
+function validate_profile_name($user_id) {
+        global $wpdb;
+    // Getting user data and user meta data
+    $err['display'] = $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE display_name = %s AND ID <> %d", $_POST['display_name'], $_POST['user_id']));
+	$err['display'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE user_login = %s AND ID <> %d", $_POST['display_name'], $_POST['user_id']));
+    $err['display'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users as users, $wpdb->usermeta as meta WHERE users.ID = meta.user_id AND meta.meta_key = 'nickname' AND meta.meta_value = %s AND users.ID <> %d", $_POST['display_name'], $_POST['user_id']));
+	
+	$err['nick']  = $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users as users, $wpdb->usermeta as meta WHERE users.ID = meta.user_id AND meta.meta_key = 'nickname' AND meta.meta_value = %s AND users.ID <> %d", $_POST['nickname'], $_POST['user_id']));
+	$err['nick'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE user_login = %s AND ID <> %d", $_POST['nickname'], $_POST['user_id']));    
+	$err['nick'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE display_name = %s AND ID <> %d", $_POST['ickname'], $_POST['user_id']));
+
+    foreach($err as $key => $e) {
+        // If display name or nickname already exists
+        if($e >= 1) {
+            $err[$key] = $_POST['username'];
+            // Adding filter to corresponding error
+            add_filter('user_profile_update_errors', "check_{$key}_field", 10, 3);
+        }
+    }
+}
+/*
+ * Filter function for display name error
+ */
+function check_display_field($errors, $update, $user) {
+        $errors->add('display_name_error',__('<span id="IL_AD9" class="IL_AD">Sorry</span>, Display Name is already in use. It needs to be unique.'));
+        return false;
+}
+/*
+ * Filter function for nickname error
+ */
+function check_nick_field($errors, $update, $user) {
+        $errors->add('display_nick_error',__('Sorry, Nickname is already in use. It needs to be unique.'));
+        return false;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Credible Names ===
+Contributors: nafsadh
+Donate link: http://example.com/
+Tags: display name, nickname, user login, unique
+Requires at least: 3.0.1
+Tested up to: 3.8
+Stable tag: 0.1.1
+License: Public Domain
+License URI:
+
+Ensure user's nickname and display name are unique and not a display name or nick name or user name of someone else
+
+== Description ==
+
+**Validate Profile Credentials and Names**
+
+In a WordPress site users have multiple names in their profile, the user login, display name and nick name. In a multi-user site, there is often a need to keep them unique. You need to allow users have only credible names, such that,
+
+* All display names are unique
+* All nicknames are unique
+* No nickname is a display name or login name of another user
+* No display name is a nickname or login name of another user
+
+Credible Names is a easy plugin to help you with that.
+
+== Installation ==
+The usual!
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php do_action('plugin_name_hook'); ?>` in your templates
+
+== Frequently Asked Questions ==
+
+= is it stable? =
+Works for me. But not rigorously tested. 
+
+== Screenshots ==
+
+None
+
+== Changelog ==
+
+
+== Upgrade Notice ==
+
+== Arbitrary section ==
+
+You may provide arbitrary sections, in the same format as the ones above.  This may be of use for extremely complicated
+plugins where more information needs to be conveyed that doesn't fit into the categories of "description" or
+"installation."  Arbitrary sections will be shown below the built-in sections outlined above.

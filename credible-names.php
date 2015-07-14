7c377461b2263f46bf93f0dcf4b8e0c329f89e55
nafsadh <nafsadh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-14 07:08:58 +0000

    updated query to check nicknames
    git-svn-id: https://plugins.svn.wordpress.org/credible-names/trunk@1198543 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- credible-names.php
+++ credible-names.php
@@ -19,37 +19,40 @@ Author URI: http://nafSadh.com/Khan
 add_action('personal_options_update', 'validate_profile_name');
 add_action('edit_user_profile_update', 'validate_profile_name');
 function validate_profile_name($user_id) {
-        global $wpdb;
-    // Getting user data and user meta data
-    $err['display'] = $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE display_name = %s AND ID <> %d", $_POST['display_name'], $_POST['user_id']));
-	$err['display'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE user_login = %s AND ID <> %d", $_POST['display_name'], $_POST['user_id']));
-    $err['display'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users as users, $wpdb->usermeta as meta WHERE users.ID = meta.user_id AND meta.meta_key = 'nickname' AND meta.meta_value = %s AND users.ID <> %d", $_POST['display_name'], $_POST['user_id']));
-	
-	$err['nick']  = $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users as users, $wpdb->usermeta as meta WHERE users.ID = meta.user_id AND meta.meta_key = 'nickname' AND meta.meta_value = %s AND users.ID <> %d", $_POST['nickname'], $_POST['user_id']));
-	$err['nick'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE user_login = %s AND ID <> %d", $_POST['nickname'], $_POST['user_id']));    
-	$err['nick'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE display_name = %s AND ID <> %d", $_POST['ickname'], $_POST['user_id']));
+  global $wpdb;
+  // Getting user data and user meta data
+  $err['display'] = $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE display_name = %s AND ID <> %d", $_POST['display_name'], $_POST['user_id']));
+  $err['display'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE user_login = %s AND ID <> %d", $_POST['display_name'], $_POST['user_id']));
+  $err['display'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users as users, $wpdb->usermeta as meta WHERE users.ID = meta.user_id AND meta.meta_key = 'nickname' AND meta.meta_value = %s AND users.ID <> %d", $_POST['display_name'], $_POST['user_id']));
 
-    foreach($err as $key => $e) {
-        // If display name or nickname already exists
-        if($e >= 1) {
-            $err[$key] = $_POST['username'];
-            // Adding filter to corresponding error
-            add_filter('user_profile_update_errors', "check_{$key}_field", 10, 3);
-        }
+  $err['nick'] = $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users as users, $wpdb->usermeta as meta WHERE users.ID = meta.user_id AND meta.meta_key = 'nickname' AND meta.meta_value = %s AND users.ID <> %d", $_POST['nickname'], $_POST['user_id']));
+  $err['nick'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE user_login = %s AND ID <> %d", $_POST['nickname'], $_POST['user_id']));
+  $err['nick'] += $wpdb->get_var($wpdb->prepare("SELECT COUNT(ID) FROM $wpdb->users WHERE display_name = %s AND ID <> %d", $_POST['nickname'], $_POST['user_id']));
+
+  foreach ($err as $key => $e) {
+    // If display name or nickname already exists
+    if ($e >= 1) {
+      $err[$key] = $_POST['username'];
+      // Adding filter to corresponding error
+      add_filter('user_profile_update_errors', "check_{$key}_field", 10, 3);
     }
+  }
 }
+
 /*
  * Filter function for display name error
  */
 function check_display_field($errors, $update, $user) {
-        $errors->add('display_name_error',__('<span id="IL_AD9" class="IL_AD">Sorry</span>, Display Name is already in use. It needs to be unique.'));
-        return false;
+  $errors->add('display_name_error', __('<span id="IL_AD9" class="IL_AD">Sorry</span>, Display Name is already in use. It needs to be unique.'));
+  return false;
 }
+
 /*
  * Filter function for nickname error
  */
 function check_nick_field($errors, $update, $user) {
-        $errors->add('display_nick_error',__('Sorry, Nickname is already in use. It needs to be unique.'));
-        return false;
+  $errors->add('display_nick_error', __('Sorry, Nickname is already in use. It needs to be unique.'));
+  return false;
 }
+
 ?>
